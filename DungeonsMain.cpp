a1413a015001a6fa1745aac9c983559fc45a70bc
trippygif <bryoelin@gmail.com>
2016-04-19 14:58:33 -0600

    updated the game interface

--- DungeonsMain.cpp
+++ DungeonsMain.cpp
@@ -8,13 +8,32 @@ using namespace std;
 int main()
 {
     int choice;
-    cout<<"=====Dungeon of the Cursed====="<<endl;
+    string start;
+    cout<<"==============================="<<endl;
+    cout<<endl;
     cout<<"Welcome to the dungeon of the cursed"<<endl;
+    cout<<endl;
+    cout<<"==============================="<<endl;
     cout<<endl;
     cout<<"Navigate through the dungeon to collect experience"<<endl;
     cout<<endl;
+    cout<<"Please type START to start the game"<<endl;
+    cout<<endl;
+    cin>>start;
+    
+    while(start != "START" && start != "start"){
+		cout<<"Command not valid"<<endl;
+		cout<<"Type START to begin"<<endl;
+		cin>>start;
+	}
+	
+	
+    cout << "\033[2J\033[1;1H";
+    cout<<endl;
     cout<<"You are in the first room. Would you like to travel left or right?"<<endl;
+    cout<<endl;
     cout<<"Enter 1 for left or 2 for right"<<endl;
+    cout<<"You can exit by pressing any other key"<<endl;
     cin>>choice;
 
 
@@ -34,4 +53,6 @@ int main()
     }
 
     cout<<"Thank you for playing! Goodbye!"<<endl;
+	
 }
+
