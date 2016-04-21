f4cec41d3293e2d510ef29390119f43c2f26e72b
William John Van Patten <William.VanPatten@colorado.edu>
2016-04-21 03:52:02 -0600

    Made the explicit gameboard, see email for a picture of the game board. The game board can be found in the code in the GameBoard.cpp file under the funciton with the title explicitInitialization()

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -9,4 +9,119 @@ GameBoard::GameBoard(int num_nodes){
 
 GameBoard::~GameBoard(){};
 
+void GameBoard::explicitInitialization()
+{
+    node *Mountains;
+    node *Cave;
+    node *Dungeon;
+    node *LavaPools;
+    node *SilverCrystalRequired;
+    node *RedCrystalRoom;
+    node *Library;
+    node *Book1;
+    node *Book2;
+    node *EmeraldMine;
+    node *GreenCrystalRoom;
+    node *ClimbMountain1;
+    node *ClimbMountain2;
+    node *ClimbMountain3;
+    node *SilverCrystalRoom;
+    node *Lake;
+    node *BlueCrystalRoom;
+
+    Mountains->parent=head;
+    Mountains->left=Cave;
+    Mountains->right=EmeraldMine;
+
+    Cave->parent=Mountains;
+    Cave->left=Dungeon;
+    Cave->right=EmeraldMine;
+
+    Dungeon->parent=Cave;
+    Dungeon->left=LavaPools;
+    Dungeon->right=Library;
+
+    LavaPools->parent=Dungeon;
+    LavaPools->left=NULL;
+    LavaPools->right=SilverCrystalRequired;
+
+    SilverCrystalRequired->parent=LavaPools;
+    SilverCrystalRequired->left=NULL;
+    SilverCrystalRequired->right=RedCrystalRoom;
+
+    RedCrystalRoom->parent=SilverCrystalRequired;
+    RedCrystalRoom->left=NULL;
+    RedCrystalRoom->right=NULL;
+
+    Library->parent=Dungeon;
+    Library->left=Book1;
+    Library->left=Book2;
+
+    EmeraldMine->parent=Cave;
+    EmeraldMine->left=NULL;
+    EmeraldMine->right=GreenCrystalRoom;
+
+    ClimbMountain1->parent=Mountains;
+    ClimbMountain1->left=NULL;
+    ClimbMountain1->right=ClimbMountain2;
+
+    ClimbMountain2->parent=ClimbMountain1;
+    ClimbMountain2->left=NULL;
+    ClimbMountain2->right=ClimbMountain3;
+
+    ClimbMountain3->parent=ClimbMountain2;
+    ClimbMountain3->left=NULL;
+    ClimbMountain3->right=NULL;
+
+    Lake->parent=head;
+    Lake->left=NULL;
+    Lake->right=BlueCrystalRoom;
+}
+
+
+
+//A function that John Van Patten made, will possibly be used in the future to search for nodes in the game board
+/*void GameBoard::createNode(node startNode,std::string nameS, std::string parentS, std::string LOR) //This function allows the programmer to create custom gameboards
+{
+     node *temp;
+     temp=startNode;
+
+    if(temp->left->name!=parentS)
+    {
+        createNode(temp->left,nameS,parentS,LOR)
+    }
+    if(temp->left->name==parentS)
+    {
+        temp=temp->left;
+        node *addNode=new node;
+        addNode->nodeName=nameS;
+        if(LOR=="L")
+        {
+            temp->left=addNode;
+        }
+        else
+        {
+
+        }
+    }
+    if(temp->right->name!=parentS)
+    {
+        createNode(temp->right,nameS,parentS,LOR)
+    }
+    if(temp->right->name==parentS)
+    {
+        temp=temp->right;
+        if(LOR=="L")
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+
+ }*/
+
 
--- GameBoard.h
+++ GameBoard.h
@@ -1,6 +1,7 @@
 #ifndef GAMEBOARD_H_
 #define GAMEBOARD_H_
 #include "Player.h"
+#include <string>
 
 
 /**
@@ -9,13 +10,13 @@
  * pertaining to the state of the game, ie experiment points, rooms, boss encounters,...
  * The game will take a parameter that is nodeCount in its initialization function this
  * will creat an array of random values % nodeCount which will then create a BST tree
- * based on these values. the player starts at head and can traverse the tree. 
+ * based on these values. the player starts at head and can traverse the tree.
  * if a leaf is reached we fight the boss in the function bossEncounter.
  */
 class GameBoard{
 
 	/**
-	 * The BST node structure this will contain all the information that that is need 
+	 * The BST node structure this will contain all the information that that is need
 	 * is needed in each room of the dungoen, ie think of these as rooms
 	 * exp = experiment point associated with enter the room for the first time
 	 * visited = bool that says if this room has been explored
@@ -25,6 +26,7 @@ class GameBoard{
 		int nodeID;
 		bool visited;
 		int exp;
+		std::string nodeName;
 		node* left;
 		node* right;
 		node* parent;
@@ -32,19 +34,19 @@ class GameBoard{
 		node(int node_id){
 			visited = false;
 			exp = 1;
-			left = nullptr;
-			right = nullptr;
-			parent = nullptr;
+			left = 0; //Changing all nullptr's to 0's so I can compile
+			right = 0;
+			parent = 0;
 			nodeID = node_id;
 		}
 	};
 
 	private:
-		
-		//head of our BST tree 
+
+		//head of our BST tree
 		node* head;
 
-		//This is the number of nodes in our BST 
+		//This is the number of nodes in our BST
 		int nodeCount;
 
 	protected:
@@ -52,14 +54,14 @@ class GameBoard{
 	public:
 
 		/* @brief constructor just takes in an int num_nodes and sets that as
-		 * the private nodeCount parameter, which sets how many nodes in 
+		 * the private nodeCount parameter, which sets how many nodes in
 		 * the dungeon.
 		 * @param int num_nodes
 		 */
 		GameBoard(int num_nodes);
 
 		/*
-		 * @brief deconstructor 
+		 * @brief deconstructor
 		 */
 		~GameBoard();
 
@@ -68,6 +70,17 @@ class GameBoard{
 		 * right, or to the current node's parent.
 		 * @param int i
 		 */
+
+
+        //A function that John Van Patten made, will possibly be used in the future to search for nodes in the game board
+        //void createNode(node startNode,std::string nameS, std::string parentS, std::string LOR)//This function allows the programmer to create custom gameboards
+
+            /*Whenever the programmer uses this function he should always make the head the start node. nameS is the string that names the node.
+            parentS is the desired parent. LOR is either L or R for what child the new node is
+            */
+
+
+
 		void traverse(int i);
 
 		/*
@@ -78,7 +91,7 @@ class GameBoard{
 		bool isLeaf(node*);
 
 		/*
-		 * @brief the boss encounter will take in player and boss health and 
+		 * @brief the boss encounter will take in player and boss health and
 		 * simulate the final boss battle based on random generated numbers
 		 * and players level to calucate how much damage you do
 		 * @param int bossHealth
@@ -93,7 +106,8 @@ class GameBoard{
 		 * @param int maxID_Size
 		 */
 		void randDungeonInitalization(int dungeonLength, int maxID_Size);
-		
+		void explicitInitialization();
+
 
 };
 
